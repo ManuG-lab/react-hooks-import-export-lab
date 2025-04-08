423331f93fde5cb7fb88c944de7b683d242c5b8f
Emmanuel Gitau <gmannu2055@gmail.com>
2025-04-08 15:07:52 +0300

    lab complete

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,11 @@
-const username = "Liza";
-const city = "New York";
+import { use } from "react";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+ export const username = "Liza";
+ export const city = "New York";
+
+ export const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+
+
+
